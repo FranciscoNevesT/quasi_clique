c3c39240f118cdd0b07892276f7b9a8b1f77c021
francisco neves <francisconevest@outlook.com>
2023-11-17 17:51:33 -0300

    Implementacao simples de uma vizinhanca-x

--- VNS.py
+++ VNS.py
@@ -53,6 +53,84 @@ def add_node(solution, graph, density, epsilon):
 
   return solution, new_density, False
 
+def calc_density(solution: set, graph: dict) -> float:
+  """
+  Calculate the density of a given solution in a graph.
+
+  Parameters:
+  - solution (set): Set of nodes representing the solution.
+  - graph (dict): Graph representation where keys are nodes and values are lists of neighbors.
+
+  Returns:
+  - float: Density of the solution in the graph.
+  """
+  if len(solution) == 1:
+    return 1
+
+  solution_size = len(solution)
+  num_edges = 0
+  num_max_edges = (solution_size * (solution_size - 1)) / 2
+
+
+  # Count the edges between nodes in the solution
+  for node in solution:
+    for node_neighbor in graph[node]:
+      if node_neighbor in solution:
+        num_edges += 1
+
+  num_edges = num_edges / 2
+
+  return num_edges / num_max_edges
+
+
+def neighborhood(solution: set, graph, base_density: float, neighborhood_number: int, epsilon: float):
+  """
+  Generate a neighborhood of a solution in a graph.
+
+  Parameters:
+  - solution (set): Current solution nodes.
+  - graph (dict): Graph representation where keys are nodes and values are lists of neighbors.
+  - base_density (float): Density of the current solution.
+  - neighborhood_number (int): Number of nodes to add/remove in the neighborhood.
+  - epsilon (float): Threshold for density, if the new density is greater than or equal to this value, the process stops.
+
+  Returns:
+  - solution_in (set): Updated solution after generating the neighborhood.
+  - density (float): Density of the updated solution.
+  - end (bool): True if the change ocorreu, False otherwise.
+  """
+  num_out_node = np.random.randint(1, min(len(solution) - 1, neighborhood_number) + 1)
+  num_in_node = neighborhood_number - num_out_node
+
+  neighbors = set()
+
+  # Find neighbors of nodes in the current solution
+  for node in solution:
+    for neighbor in graph[node]:
+      if neighbor not in solution:
+        neighbors.add(neighbor)
+
+  solution_in = solution.copy()
+  solution = solution.copy()
+
+  # Randomly remove nodes from the solution
+  nodes_out = np.random.choice(list(solution_in), num_out_node, replace=False)
+  for node in nodes_out:
+    solution_in = solution_in - {node}
+
+  # Randomly add nodes from the neighbors
+  nodes_in = np.random.choice(list(neighbors), num_in_node, replace=False)
+  for node in nodes_in:
+    solution_in.add(node)
+
+  density = calc_density(solution_in, graph)
+
+  # Check if the new density is greater than or equal to the threshold
+  if density >= epsilon:
+    return solution_in, density, True
+  else:
+    return solution, base_density, False
+
 # Get the graph from the specified path
 graph = get_graph(path="data/johnson8-2-4.mtx")
 
@@ -60,14 +138,28 @@ graph = get_graph(path="data/johnson8-2-4.mtx")
 solution = set('1')
 density = 1
 
-while True:
-  # Update the solution, density, and check if the process should stop
-  solution, density, end = add_node(solution, graph, density=density, epsilon=0.9)
+best_solution = set()
 
-  if end:
-    break
+c = 1000
+for _ in range(100):
+  while True:
+    # Update the solution, density, and check if the process should stop
+    solution, density, end = add_node(solution, graph, density=density, epsilon=0.9)
 
+    if end:
+      break
+
+  if len(solution) > len(best_solution):
+    best_solution = solution.copy()
+
+  print(solution)
+  change = False
+  c = 1000
+  while change == False and c > 0:
+    solution, density, change = neighborhood(solution,graph,base_density = density,neighborhood_number = 2,epsilon=0.9)
+    c -= 1
+
+density = calc_density(best_solution,graph)
 # Print the final solution, density, and end status
-print("Final Solution:", solution)
+print("Final Solution:", best_solution)
 print("Final Density:", density)
-print("Process Ended:", end)
