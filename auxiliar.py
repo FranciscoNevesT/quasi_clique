c3d502d36036aa9cbe840daf5646d0e29f305e4b
francisco neves <francisconevest@outlook.com>
2023-11-28 18:16:54 -0300

    Versao utilizada nos testes

--- auxiliar.py
+++ auxiliar.py
@@ -4,10 +4,15 @@ def get_graph(path):
   with open(path,"r") as file:
     text = file.read()
 
-  text = text.split("\n")[2:-1]
+  text_ = []
 
-  for p in text:
-    i,j = p.split(" ")
+  for t in text.split("\n"):
+    tipo = t.split(" ")[0]
+    if tipo == "e":
+      text_.append(t)
+
+  for p in text_:
+    t,i,j = p.split(" ")
 
     nodes = graph.setdefault(i,[])
     nodes.append(j)
--- main.py
+++ main.py
@@ -5,26 +5,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 num_sample = 5
-epsilon = 0.8
+epsilon = 0.95
 neighborhood_x = [2,3,4]
 num_repetitions = 1000
 
 data = []
 for path in os.listdir("data"):
+  print(path)
   if path == "vazio":
     continue
 
   graph = get_graph(path="data/{}".format(path))
 
+  best_solution = None
+  best_density = None
   for _ in range(num_sample):
-    best_solution,density = vns(graph,epsilon=epsilon,num_repetitions=num_repetitions,neighborhood_x=neighborhood_x)
-    data.append([path,len(best_solution),density])
+    solution,density = vns(graph,epsilon=epsilon,num_repetitions=num_repetitions,neighborhood_x=neighborhood_x)
+
+    if best_solution is None or len(solution) > len(best_solution):
+      best_solution = solution
+      best_density = density
+    elif len(solution) == len(best_solution) and density > best_density:
+      best_solution = solution
+      best_density = density
+
+  data.append([path,len(best_solution),best_density])
 
 data = pd.DataFrame(data,columns=["example","num_nodes","density"])
-sns.boxplot(data = data, x = "example", y = "num_nodes")
-plt.show()
-
-sns.boxplot(data = data, x = "example", y = "density")
-plt.show()
-
+data.to_csv("results/solution_{}.csv".format(epsilon),index=False)
 print(data)
